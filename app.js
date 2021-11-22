0cd832c0f13df1f4df0bc26b004e79124d672ba0
JonnathanLenovo <andres.652@hotmail.com>
2021-11-22 18:47:05 -0500

    async patterns

--- app.js
+++ app.js
@@ -1,22 +1,73 @@
-// npm -global command, comes with node
-// npm --version
+/////   PROMISE PATTERN 3:
+const { readFile, writeFile } = require("fs").promises;
 
-// local dependency - use it only in this particular project
-// npm i <packageName>
+const start = async () => {
+  try {
+    const first = await readFile("./content/first.txt", "utf-8");
+    const second = await readFile("./content/second.txt", "utf-8");
+    console.log(first, second);
+    await writeFile(
+      "./content/result-mind-grenade.txt",
+      `THIS IS AWESOME: ${first} ${second}`,
+      { flag: "a" }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (mac)
+start();
 
 
-// package.json - manifest file (stores important info about project/package)
-// manual approach (create json in the root, create properties etc)
-// npm init (step by step, press enter to skip)
-// npm init -y (everything default)
+/////   PROMISE PATTERN 2:
+// const { readFile, writeFile } = require("fs");
+// const util = require("util"); //module from node
+
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
+
+// const start = async () => {
+//   try {
+//     const first = await readFilePromise("./content/first.txt", "utf-8");
+//     const second = await readFilePromise("./content/second.txt", "utf-8");
+//     console.log(first, second);
+//     await writeFilePromise(
+//       "./content/result-mind-grenade.txt",
+//       `THIS IS AWESOME: ${first} ${second}`,
+//       { flag: "a" }
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// start();
 
 
-const _ = require('lodash')
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items);
-console.log(newItems)
-console.log('hello people')
+/////   PROMISE PATTERN 1:
+// const { readFile, writeFile } = require("fs");
+// const getText = (path) => {
+//     return new Promise((resolve, reject) => {
+//       //return a callback function
+//       readFile(path, "utf-8", (err, data) => {
+//         if (err) {
+//           reject(err);
+//         }
+//         // console.log(data)
+//         resolve(data);
+//       });
+//     });
+//   };
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+// const start = async () => {
+//     try {
+//       const first = await getText("./content/first.txt");
+//       const second = await getText("./content/second.txt");
+//       console.log(first, second)
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+// start();
