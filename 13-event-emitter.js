ff42bf4c852a8d5d20c078f6ac36cd78d04036a6
JonnathanLenovo <andres.652@hotmail.com>
2021-11-22 19:04:17 -0500

    emit event with extra arguments

--- 13-event-emitter.js
+++ 13-event-emitter.js
@@ -4,8 +4,13 @@ const EventEmitter = require('events');
 
 const customEmitter = new EventEmitter()
 
-customEmitter.on('response', ()=> {
-    console.log(`data received`)
+//the order between .on and .emit matters:
+customEmitter.on('response', (name, id)=> {
+    console.log(`data received ${name} with id: ${id}`)
 })
 
-customEmitter.emit('response')
+customEmitter.on('response', ()=> {
+    console.log(`some other logic here`)
+})
+
+customEmitter.emit('response', 'john', 34)
